01c7a638195f62649eda1a6fd91c7c11bf5a411c
Lionel Jullien <lionel.jullien@hpe.com>
2020-07-06 12:26:11 +0200

    New Terraform scenarios

---
+++ Terraform/create-profile.tf
@@ -0,0 +1,30 @@
+
+provider "oneview" {
+  ov_domain = "Local"
+  ov_username = "Administrator"
+  ov_password = "password"
+  ov_endpoint = "https://192.168.56.101"
+  ov_sslverify = false
+  ov_apiversion = 1600
+  ov_ifmatch = "*"
+}
+ 
+// Get Server Hardware
+data "oneview_server_hardware" "server_hardware" {
+  name = "Synergy-Encl-1, bay 3"
+}
+
+// Get Server Profile Template
+data "oneview_server_profile_template" "server_profile_template" {
+  name = "HPE Synergy 660 Gen9 with Local Boot and SAN Storage for Windows Template"
+}
+
+// Create Server Profile from Server Profile Template
+resource "oneview_server_profile" "server_profile" {
+  name          = "Profile-4"
+  template      = "${data.oneview_server_profile_template.server_profile_template.name}"
+  hardware_name = "${data.oneview_server_hardware.server_hardware.name}"
+  type          = "ServerProfileV12"
+  power_state = "on"
+}
+
